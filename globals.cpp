03ab63738b6025537d0090800621ea58e736565c
Dan Adrian Mirea <damirea84@gmail.com>
2024-07-15 13:32:47 +0300

    update globals

--- globals.cpp
+++ globals.cpp
@@ -9,6 +9,18 @@ int cellSize = 30;
 int cellCount = 25;
 int offset = 75;
 
+Color grey = Color{ 29, 29, 27, 255 };
+Color yellow = Color{ 243, 216, 63, 255 };
+int windowWidth = 1920;
+int windowHeight = 1080;
+const int gameScreenWidth = 800;
+const int gameScreenHeight = 800;
+bool exitWindowRequested = false;
+bool exitWindow = false;
+bool fullscreen = true;
+float borderOffsetWidth = 20.0;
+float borderOffsetHeight = 50.0f;
+
 bool ElementInDeque(Vector2 element, deque<Vector2> deque)
 {
     for (unsigned int i = 0; i < deque.size(); i++)
--- globals.h
+++ globals.h
@@ -14,3 +14,14 @@ extern int offset;
 
 extern bool ElementInDeque(Vector2 element, deque<Vector2> deque);
 
+extern Color grey;
+extern Color yellow;
+extern int windowWidth;
+extern int windowHeight;
+extern const int gameScreenWidth;
+extern const int gameScreenHeight;
+extern bool exitWindow;
+extern bool exitWindowRequested;
+extern bool fullscreen;
+extern float borderOffsetWidth;
+extern float borderOffsetHeight;
